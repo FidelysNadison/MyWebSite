bee3463306f51850045c42426b4e64f01a368afd
FidelysNadison <nadisonsolojeanfidelys@gmail.com>
2022-12-06 16:59:02 +0100

    début du components Parcours

--- src/components/Parcours.jsx
+++ src/components/Parcours.jsx
@@ -3,8 +3,23 @@ import React from "react";
 export default function Navbar() {
   return (
     <div className="container px-5 py-10 mx-auto text-center lg:px-40">
-      <div className="">
-        
+      <div id="Formations">
+      <h1> Formations </h1>
+        <ul>
+          <li>ESGI</li>
+          <li>BeCode</li>
+          <li>OpenClassroom</li>
+          <li>E-media</li>
+          <li>NextA</li>
+        </ul>
+      </div>
+
+      <div id="Expérience">
+        <h1> Expérience</h1>
+        <ul>
+          <li>HaFe-Event</li>
+          <li>Mada- Food</li>
+        </ul>
       </div>
     </div>
   );
