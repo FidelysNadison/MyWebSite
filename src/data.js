5e4411bbacb373111ecc557dc73b00b08bfeed02
FidelysNadison <nadisonsolojeanfidelys@gmail.com>
2022-12-12 12:31:33 +0100

    modification des data pour la page parcours

--- src/data.js
+++ src/data.js
@@ -49,9 +49,62 @@ export const skills = [
 ];
 
 export const parcours = [
-  "ESGI",
-  "BeCode",
-  "OpenClassroom",
-  "E-Media",
-  "Nexta",
+  {
+    title: "ESGI",
+    subtitle: "1er année Bachelor en informatique",
+    image: "./esgi.png",
+    powers: [
+      "Développer et modéliser des Bases de données (Langages C Algorithme et langage SQL)",
+      "Développer un site Web (HTML, CSS, PHP, JavaScript et Web API Fetch)",
+      "Gérer des systèmes de réseaux informatiques (Linux et Windows)",
+      "Analyser des process de gestion d ‘entreprise (VBA Excel)",
+    ]
+  },
+
+  {
+    title: "BeCode",
+    subtitle: "Développeur Web Junior",
+    image: "./BeCode.png",
+    powers: [
+      "Programmer dans des langages informatiques spécifiques (Html, Css,JavaScript, PhP et MySql",
+      "Travailler en équipe et en mode projet (GitHub, Commandes Git,Trello)",
+      "Présenter et convaincre de la pertinence de la solution développée.",
+      "Utiliser des frameworks ( Boostrap , Figma )",
+      "Créer des site web à l’aide du librairie REACT",
+    ]
+  },
+
+  {
+    title: "OpenClassroom",
+    subtitle: "Développeur Web",
+    image: "./Opencalssrooms.png",
+    powers: [
+      "Créer un site Web avec Html et Css",
+      "Utiliser les commandes dans un terminal",
+      "Ecrire du JavaScript pour le Web",
+      "Concevoir un site Web avec PhP et MySql",
+    ]
+  },
+
+  {
+    title: "E-Media",
+    subtitle: "Communication Audiovisuel et Numérique",
+    image: "./E-media.png",
+    powers: [
+      "Préparer et réaliser des prises de vues photographiques.",
+      "Cadrer des vidéos et concevoir un mini-reportage (Adobe Premier Pro et After Effect)",
+      "Développer un site web statique avec Html et Css",
+    ]
+  },
+
+  {
+    title: "Nexta",
+    subtitle: "Rédaction web et Marketing digital",
+    image: "./NEXTA.png",
+    powers: [
+      "Animer une communauté web (Community Management)",
+      "Alimenter des réseaux sociaux et traiter les informations",
+      "Évaluer l'e-réputation d'un site web",
+    ]
+  },
 ];
