384caa9f046af63045f430fca75941851f4f75ea
Fidelys Nadison <nadisonsolojeanfidelys@gmail.com>
2023-05-17 00:12:12 +0200

    modification mail adress

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -50,7 +50,7 @@ export default function Contact() {
                 EMAIL
               </h2>
               <a className="text-indigo-400 leading-relaxed">
-                nadisonsolojeanfidelys@gmail.com
+                snadison@outlook.fr
               </a>
               <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">
                 PHONE
