899088e02b20f06cb3042f02af1f59d7b482e151
Fidelys Nadison <nadisonsolojeanfidelys@gmail.com>
2023-05-22 19:59:20 +0200

    modification lien vers projet portfolio

--- src/data.js
+++ src/data.js
@@ -29,7 +29,7 @@ export const projects = [
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
     image: "./MyWebSite.png",
-    link: "https://github.com/FidelysNadison?tab=repositories",
+    link: "https://github.com/FidelysNadison/MyWebSite",
   },
 ];
 
