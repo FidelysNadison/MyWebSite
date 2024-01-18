a37a3364ff4eb011666c7b437d879f80710f3cb6
nadison <fidelys.nadison@alliantic.com>
2024-01-18 20:58:02 +0100

    Mise à jour

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -32,7 +32,7 @@ export default function About() {
           <img
             className="object-cover object-center rounded"
             alt="hero"
-            src="./Avatar.svg"
+            src="./pdp.png"
           />
         </div>
       </div>
